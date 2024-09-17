97b773a1b4f58dda06049c58b7111d242efecda3
masella <gio.masella17@gmail.com>
2024-09-17 10:04:02 -0400

    add global exception handling

---
+++ src/main/java/com/gmasella/banking/exception/AccountException.java
@@ -0,0 +1,8 @@
+package com.gmasella.banking.exception;
+
+public class AccountException extends RuntimeException{
+
+    public AccountException(String message){
+        super(message);
+    }
+}
--- src/main/java/com/gmasella/banking/service/impl/AccountServiceImpl.java
+++ src/main/java/com/gmasella/banking/service/impl/AccountServiceImpl.java
@@ -2,6 +2,7 @@ package com.gmasella.banking.service.impl;
 
 import com.gmasella.banking.dto.AccountDTO;
 import com.gmasella.banking.entity.Account;
+import com.gmasella.banking.exception.AccountException;
 import com.gmasella.banking.mapper.AccountMapper;
 import com.gmasella.banking.repository.AccountRepository;
 import com.gmasella.banking.service.AccountService;
@@ -34,7 +35,7 @@ public class AccountServiceImpl implements AccountService {
 
         Account account = accountRepository
                 .findById(id)
-                .orElseThrow(()-> new RuntimeException("Account does not exist"));
+                .orElseThrow(()-> new AccountException("Account does not exist"));
 
         return AccountMapper.mapToAccountDTO(account);
     }
@@ -44,7 +45,7 @@ public class AccountServiceImpl implements AccountService {
 
         Account account = accountRepository
                 .findById(id)
-                .orElseThrow(()-> new RuntimeException("Account does not exist"));
+                .orElseThrow(()-> new AccountException("Account does not exist"));
         double total = account.getBalance() + amount;
         account.setBalance(total);
         Account savedAccount = accountRepository.save(account);
@@ -55,7 +56,7 @@ public class AccountServiceImpl implements AccountService {
     public AccountDTO withdraw(Long id, double amount) throws Exception {
         Account account = accountRepository
                 .findById(id)
-                .orElseThrow(()-> new RuntimeException("Account does not exist"));
+                .orElseThrow(()-> new AccountException("Account does not exist"));
 
         if(account.getBalance() < amount){
             throw new RuntimeException("Insufficient Funds!");
@@ -80,7 +81,7 @@ public class AccountServiceImpl implements AccountService {
 
         Account account = accountRepository
                 .findById(id)
-                .orElseThrow(()-> new RuntimeException("Account does not exist"));
+                .orElseThrow(()-> new AccountException("Account does not exist"));
 
         accountRepository.deleteById(id);
     }
